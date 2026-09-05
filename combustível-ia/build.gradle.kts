3a5ee4d713dc5cc29bf4ce644d9e7e833eb7d26c
Fabricio Rocha Carvalho <fabricio.carvalho974@cs.up.edu.br>
2026-09-05 10:35:18 -0300

    Update build.gradle.kts

--- combustível-ia/build.gradle.kts
+++ combustível-ia/build.gradle.kts
@@ -1,9 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+
   alias(libs.plugins.android.application) apply false
   alias(libs.plugins.kotlin.compose) apply false
   alias(libs.plugins.google.devtools.ksp) apply false
   alias(libs.plugins.roborazzi) apply false
   alias(libs.plugins.secrets) apply false
-  alias(libs.plugins.google.services) apply false
+  alias(libs.plugins.google.services) apply falseplugins {
+    // ... outros plugins que já existam aí
+    id("com.google.gms.google-services") version "4.4.2" apply false
+
 }
