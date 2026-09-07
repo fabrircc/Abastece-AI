9f943288a6ea951527e4e52c10760dd688a446f1
Fabricio Rocha Carvalho <fabricio.carvalho974@cs.up.edu.br>
2026-09-07 12:02:27 -0300

    Update build.gradle.kts

--- combustível-ia/build.gradle.kts
+++ combustível-ia/build.gradle.kts
@@ -1,25 +1,5 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-
-  alias(libs.plugins.android.application) apply false
-  alias(libs.plugins.kotlin.compose) apply false
-  alias(libs.plugins.google.devtools.ksp) apply false
-  alias(libs.plugins.roborazzi) apply false
-  alias(libs.plugins.secrets) apply false
-  alias(libs.plugins.google.services) apply falseplugins {
-    // ... outros plugins que já existam aí
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
-plugins {
-    id("com.android.application")
-    id("com.google.gms.google-services") // <-- Adicione esta linha aqui
-    // ... outros plugins
-dependencies {
-    // Importa o Firebase BoM
-    implementation(platform("com.google.firebase:firebase-bom:33.8.0"))
-    
-    // Produtos do Firebase que vamos usar
-    implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.firebase:firebase-firestore")
-
-
 }
